d820f63bcb3179a5ca7ed176d92bfbb181208907
woai3c <411020382@qq.com>
2019-04-17 13:31:16 +0800

    add vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://xxxx/device/', //对应自己的接口
+                changeOrigin: true,
+                ws: true,
+                pathRewrite: {
+                  '^/api': ''
+                }
+            }
+        }
+    }
+  }
