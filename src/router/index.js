4d84b3a7ac4dbf824e75b68c0c55e70868f5e8c7
woai3c <411020382@qq.com>
2019-06-18 17:11:39 +0800

    优化utils文件

--- src/router/index.js
+++ src/router/index.js
@@ -14,4 +14,33 @@ const router = new Router({
     ]   
 })
 
+// 需要通过后台数据来生成的组件 请写在这
+export const routes = {
+    'home': {
+        path: 'home',
+        name: 'home',
+        component: () => import('../views/Home.vue')
+    },
+    't1': {
+        path: 't1',
+        name: 't1',
+        component: () => import('../views/T1.vue')
+    },
+    'password': {
+        path: 'password',
+        name: 'password',
+        component: () => import('../views/Password.vue')
+    },
+    'msg': {
+        path: 'msg',
+        name: 'msg',
+        component: () => import('../views/Msg.vue')
+    },
+    'userinfo': {
+        path: 'userinfo',
+        name: 'userinfo',
+        component: () => import('../views/UserInfo.vue')
+    }
+}
+
 export default router
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,31 +1,4 @@
-// 首页下的子组件
-const routes = {
-    'home': {
-        path: 'home',
-        name: 'home',
-        component: () => import('../views/Home.vue')
-    },
-    't1': {
-        path: 't1',
-        name: 't1',
-        component: () => import('../views/T1.vue')
-    },
-    'password': {
-        path: 'password',
-        name: 'password',
-        component: () => import('../views/Password.vue')
-    },
-    'msg': {
-        path: 'msg',
-        name: 'msg',
-        component: () => import('../views/Msg.vue')
-    },
-    'userinfo': {
-        path: 'userinfo',
-        name: 'userinfo',
-        component: () => import('../views/UserInfo.vue')
-    }
-}
+import { routes } from '../router'
 
 // 将菜单信息转成对应的路由信息 动态添加
 export function menusToRoutes(data) {
