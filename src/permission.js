9426b0668a871fb4a7ed11778c860d4dd9e141bc
woai3c <411020382@qq.com>
2019-07-24 10:53:46 +0800

    页面跳转时添加进度条

--- src/permission.js
+++ src/permission.js
@@ -1,10 +1,13 @@
 import router from './router'
 import store from './store'
-import { menusToRoutes } from './utils'
+import { menusToRoutes, resetTokenAndClearUser } from './utils'
+import { LoadingBar } from 'iview'
+import axios from 'axios'
 
 // 是否有菜单数据
 let hasMenus = false
 router.beforeEach(async (to, from, next) => {
+    LoadingBar.start()
     if (localStorage.getItem('token')) {
         if (to.path === '/login') {
             next({path: '/'})
@@ -14,12 +17,15 @@ router.beforeEach(async (to, from, next) => {
             } else {
                 try {
                     // 这里可以用 await 配合请求后台数据来生成路由
+                    // const data = await axios.get('xxx')
+                    // const routes = menusToRoutes(data)
                     const routes = menusToRoutes(store.state.menuItems)
                     // 动态添加路由
                     router.addRoutes(routes)
                     hasMenus = true
                     next({path: to.path || '/'})
                 } catch (error) {
+                    resetTokenAndClearUser()
                     next(`/login?redirect=${to.path}`)
                 }
             }
@@ -32,4 +38,8 @@ router.beforeEach(async (to, from, next) => {
             next(`/login?redirect=${to.path}`)
         }
     }
+})
+
+router.afterEach(() => {
+    LoadingBar.finish()
 })
